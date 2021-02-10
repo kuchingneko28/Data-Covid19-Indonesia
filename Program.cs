4059814d930aef8fd5e523fe03b3c0b7eb34e790
kuchingneko28 <kuchingneko28@gmail.com>
2021-02-11 00:48:24 +0800

    second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -14,7 +16,7 @@ namespace Data_Covid_Indonesia
          //Url api
          await getUrl("https://data.covid19.go.id/public/api/prov.json");   
         }
-        static async Task<string> getUrl(string x)
+        static async Task<object> getUrl(string x)
         {
             string url = x;
             // Driver
@@ -35,58 +37,10 @@ namespace Data_Covid_Indonesia
              Console.WriteLine("- Penambahan positif: " + data.penambahan.positif);
              Console.WriteLine("- Penambahan sembuh: " + data.penambahan.sembuh);
              Console.WriteLine("- Penambahan meninggal: " + data.penambahan.meninggal + "\n");
-             
-            }
-            
+             }
+   
             return url;
         }
-        
-    }
-    public class JenisKelamin    {
-        public string key { get; set; } 
-        public int doc_count { get; set; } 
-    }
-
-    public class Usia    {
-        public double value { get; set; } 
-    }
-
-    public class KelompokUmur    {
-        public string key { get; set; } 
-        public int doc_count { get; set; } 
-        public Usia usia { get; set; } 
-    }
-
-    public class Lokasi    {
-        public double lon { get; set; } 
-        public double lat { get; set; } 
-    }
-
-    public class Penambahan    {
-        public int positif { get; set; } 
-        public int sembuh { get; set; } 
-        public int meninggal { get; set; } 
-    }
-
-    public class ListData    {
-        public string key { get; set; } 
-        public double doc_count { get; set; } 
-        public int jumlah_kasus { get; set; } 
-        public int jumlah_sembuh { get; set; } 
-        public int jumlah_meninggal { get; set; } 
-        public int jumlah_dirawat { get; set; } 
-        public List<JenisKelamin> jenis_kelamin { get; set; } 
-        public List<KelompokUmur> kelompok_umur { get; set; } 
-        public Lokasi lokasi { get; set; } 
-        public Penambahan penambahan { get; set; } 
-    }
-
-    public class AllData    {
-        public string last_date { get; set; } 
-        public double current_data { get; set; } 
-        public double missing_data { get; set; } 
-        public int tanpa_provinsi { get; set; } 
-        public List<ListData> list_data { get; set; } 
         
     }
 }
